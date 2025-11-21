b8bfbf0ee26a2282d81bf332816af8535bfc76a4
Itss-Rio <rioyblack@gmail.com>
2025-11-21 12:44:24 +0100

    Solved error bug map. I think

--- JavaScript/script-dino.js
+++ JavaScript/script-dino.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const gameArea = document.getElementById("game-area-dino");
     const canvas = document.createElement("canvas");
     canvas.width = 800;
-    canvas.height = 400;
+    canvas.height = 250;
     canvas.style.borderRadius = "25px";
     gameArea.innerHTML = "";
     gameArea.appendChild(canvas);
