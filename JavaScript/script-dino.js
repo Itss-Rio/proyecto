d45c04dd3234c849762e045522087d8ce4f1ceb6
Itss-Rio <rioyblack@gmail.com>
2025-11-21 12:56:47 +0100

    Better map

--- JavaScript/script-dino.js
+++ JavaScript/script-dino.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const gameArea = document.getElementById("game-area-dino");
     const canvas = document.createElement("canvas");
     canvas.width = 800;
-    canvas.height = 225;
+    canvas.height = 200;
     canvas.style.borderRadius = "25px";
     gameArea.innerHTML = "";
     gameArea.appendChild(canvas);
